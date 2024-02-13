7cbaff09beb2f920268cc3acde52d0f96b4a7fae
Hayden Stainsby <hds@caffeineconcepts.com>
2024-02-14 00:12:34 +0100

    feat(replay): add dynamic span creation
    It is now possible to create a span dynamically. It uses the same
    metadata store and metadata creation as for the event.
    
    It seems that simply creating a span is enough for the subscriber to
    receive a `new` event for that span.
    
    Once a span is created, the rest of the span lifecycle works as normal.
    So the subscriber receives the `enter`, `exit`, and `close` events as
    normal.

--- tracing-replay/src/lib.rs
+++ tracing-replay/src/lib.rs
@@ -4,20 +4,28 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use tracing::{info_span, Span};
 use tracing_core::{field, Event, Metadata};
 
 pub fn crimes() {
     let metadata_store: Arc<Mutex<HashMap<u64, Metadata<'static>>>> =
         Arc::new(Mutex::new(HashMap::new()));
 
+    let _span = info_span!("Span over them all", happy = true).entered();
+
     tracing::info!("This is an `info!` macro event");
 
-    write_event(1, "This is a hand-rolled event", metadata_store.clone());
-    write_event(
-        1,
-        "This is another hand-rolled event, but with the same metadata",
-        metadata_store.clone(),
-    );
+    {
+        let span = make_span(3, "hand-span", metadata_store.clone());
+        let _guard = span.enter();
+
+        write_event(1, "This is a hand-rolled event", metadata_store.clone());
+        write_event(
+            1,
+            "This is another hand-rolled event with the same metadata",
+            metadata_store.clone(),
+        );
+    }
     write_event(
         2,
         "This hand-rolled event has different metadata",
@@ -32,6 +40,7 @@ enum MetadataEntry {
 
 fn metadata_or_create(
     metadata_id: u64,
+    name: &'static str,
     store: Arc<Mutex<HashMap<u64, Metadata<'static>>>>,
 ) -> MetadataEntry {
     let mut guard = store.lock().unwrap();
@@ -40,6 +49,7 @@ fn metadata_or_create(
     let metadata = (*guard).entry(metadata_id).or_insert_with(|| {
         new_metadata = true;
         let metadata: Metadata<'static> = make_metadata_with_level(
+            name,
             tracing_core::Level::INFO,
             TryInto::<u32>::try_into(metadata_id).unwrap() * 5 + 100,
         );
@@ -61,7 +71,7 @@ fn metadata_or_create(
 
 fn write_event(metadata_id: u64, msg: &str, store: Arc<Mutex<HashMap<u64, Metadata<'static>>>>) {
     let _enabled = tracing::dispatcher::get_default(move |dispatch| {
-        let metadata = match metadata_or_create(metadata_id, store.clone()) {
+        let metadata = match metadata_or_create(metadata_id, "", store.clone()) {
             MetadataEntry::New(metadata) => {
                 dispatch.register_callsite(metadata);
                 metadata
@@ -83,7 +93,31 @@ fn write_event(metadata_id: u64, msg: &str, store: Arc<Mutex<HashMap<u64, Metada
     });
 }
 
+fn make_span(
+    metadata_id: u64,
+    span_name: &'static str,
+    store: Arc<Mutex<HashMap<u64, Metadata<'static>>>>,
+) -> Span {
+    tracing::dispatcher::get_default(move |dispatch| {
+        let metadata = match metadata_or_create(metadata_id, span_name, store.clone()) {
+            MetadataEntry::New(metadata) => {
+                dispatch.register_callsite(metadata);
+                metadata
+            }
+            MetadataEntry::Existing(metadata) => metadata,
+        };
+
+        let fields = metadata.fields();
+        let field = fields.field("field").unwrap();
+        let values = [(&field, Some(&"field-value" as &dyn field::Value))];
+        let value_set = metadata.fields().value_set(&values);
+
+        Span::new(metadata, &value_set)
+    })
+}
+
 fn make_metadata_with_level(
+    name: &'static str,
     level: tracing_core::Level,
     line_number: u32,
 ) -> tracing::Metadata<'static> {
@@ -102,13 +136,13 @@ fn make_metadata_with_level(
 
     let cs: &'static Cs = Box::leak(Box::new(Cs::new()));
     tracing::Metadata::new(
-        "",
+        name,
         "doing-crimes",
         level,
         Some("src/crimes.rs"),
         Some(line_number),
         None,
-        tracing::field::FieldSet::new(&["message"], tracing_core::identify_callsite!(cs)),
+        tracing::field::FieldSet::new(&["message", "field"], tracing_core::identify_callsite!(cs)),
         tracing::metadata::Kind::EVENT,
     )
 }
